a61b0a37eb5e2b304a07f587f6f2ff6ddbfc4fdf
igor hirsch <g1rsh.egor@gmail.com>
2025-03-08 11:46:41 +0100

    1. First End point.  VillaDTO, Data Store

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaAPI.Controllers.Models;
+using MagicVilla_VillaAPI.Controllers.Models.Dto;
+using MagicVilla_VillaAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    // [Route("api/[controller]")]
+    [Route("api/villaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("id")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,17 @@
+﻿using MagicVilla_VillaAPI.Controllers.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO { Id = 1, Name = "Villa 1" },
+            new VillaDTO { Id = 2, Name = "Villa 2" },
+            new VillaDTO { Id = 3, Name = "Villa 3" },
+            new VillaDTO { Id = 4, Name = "Villa 4" },
+            new VillaDTO { Id = 5, Name = "Villa 5" }
+        };
+
+    }
+}
