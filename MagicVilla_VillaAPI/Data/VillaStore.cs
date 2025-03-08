d55e50ab067f75701039f58a934d453af98a9954
igor hirsch <g1rsh.egor@gmail.com>
2025-03-08 14:03:27 +0100

    2. CRUD Functionality. Postman

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -6,11 +6,8 @@ namespace MagicVilla_VillaAPI.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>
         {
-            new VillaDTO { Id = 1, Name = "Villa 1" },
-            new VillaDTO { Id = 2, Name = "Villa 2" },
-            new VillaDTO { Id = 3, Name = "Villa 3" },
-            new VillaDTO { Id = 4, Name = "Villa 4" },
-            new VillaDTO { Id = 5, Name = "Villa 5" }
+            new VillaDTO { Id = 1, Name = "Villa 1", Sqft=100, Occupancy=4 },
+            new VillaDTO { Id = 2, Name = "Villa 2", Sqft = 100, Occupancy=3 },
         };
 
     }
