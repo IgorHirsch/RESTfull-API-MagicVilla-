a900ad30903cfce1ebb1121bcd3b35ba61229e56
igor hirsch <g1rsh.egor@gmail.com>
2025-03-08 20:17:34 +0100

    3. Dependency Injection and DBContext

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace MagicVilla_VillaAPI.Controllers.Models.Dto
+namespace MagicVilla_VillaAPI.Models.Dto
 {
     public class VillaDTO
     {
@@ -9,8 +9,15 @@ namespace MagicVilla_VillaAPI.Controllers.Models.Dto
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
+        public string Details { get; set; }
 
+        [Required]
+        public double Rate { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+
     }
 }
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,14 +1,19 @@
+
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers(option => { option.ReturnHttpNotAcceptable = true; }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-
-
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<ApplicationDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllers(option => {
+    //option.ReturnHttpNotAcceptable=true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
